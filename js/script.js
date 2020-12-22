dde0fad0261766a55063ec9bf6999baf5cfe2659
Pierre-Yves Flamand <46058408+PierreYvesFlamand@users.noreply.github.com>
2020-12-22 01:44:02 +0100

    Added starting modal

--- js/script.js
+++ js/script.js
@@ -1,6 +1,38 @@
-start_game();
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        document.querySelector('.modal').style.display = 'none';
+        toggleModal('start');
+    }
+};
+
+const toggleModal = (modal = null) => {
+    const modalDiv = document.querySelector('.modal');
+    modalDiv.style.display = modalDiv.style.display === 'none' ? 'flex' : 'none';
+
+    console.log(modals[modal]);
+
+    document.querySelector('.modal-card').innerHTML = modal ? modals[modal] : '';
+};
+
+const modals = {
+    start: `
+        <h1>Chess</h1>
+        <h2>How to play</h2>
+        <ul>
+            <li>Hover a pawn to know witch move it can performs</li>
+            <li>Click a pawn and then a square to move the pawn</li>
+        </ul>
+        <h2>Rules availables</h2>
+        <ul>
+            <li>Pawn promotion (reach ennemy last first row with a pawn to promote it)</li>
+            <li>Castling (click the king to perform it)</li>
+        </ul>
+        <button onclick="start_game()">Start the game</button>
+    `,
+};
 
 function start_game() {
+    toggleModal();
     pawn_sellection(true);
 }
 
